5f0f71ce092a67c7c59059c2e4e3988da63a6d16
Ivan Bohash <iv.bohash@gmail.com>
2025-04-21 21:47:45 +0200

    add docstring and comments

--- manage.py
+++ manage.py
@@ -2,15 +2,15 @@ from app.run import PeriodicTask
 from app.periodic_tasks.jobs import scheduler_jobs
 
 
-def run_scheduler():
-    runner = PeriodicTask()
+# def run_scheduler():
+#     runner = PeriodicTask()
+#
+#     for job in scheduler_jobs:
+#         runner.add_task(job)
+#
+#     runner.run()
 
-    for job in scheduler_jobs:
-        runner.add_task(job)
-
-    runner.run()
-
-
-if __name__ == "__main__":
-    run_scheduler()
+#
+# if __name__ == "__main__":
+#     run_scheduler()
 
